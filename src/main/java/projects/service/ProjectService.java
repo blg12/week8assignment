8c689f8f8b0d568a0a18d788835093874ef19b3b
blg12 <bradygreer12@gmail.com>
2023-05-27 03:33:22 -0500

    week 10

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,5 +1,8 @@
 package projects.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
@@ -8,10 +11,21 @@ public class ProjectService {
 	
 	
 	private ProjectDao projectDao = new ProjectDao();
+	
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() 
+				-> new NoSuchElementException("Project with project ID=" + projectId
+						+ " does not exist."));
+	}
 
 	public Project addProject(Project project) {
 		
 		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	
 	}
 
 }
